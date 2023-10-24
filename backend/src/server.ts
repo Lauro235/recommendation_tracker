6feffea434d4defed6b751b3564eceb69bb362b6
Lauro235 <lorentzbloomguitar@gmail.com>
2023-10-24 16:06:59 +0100

    Added example.env file - ready to test backend deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 const port = 3000;
+
+console.log(process.env.my_secret)
 
 app.listen(port, () => {
   console.log(`Backend API Listening on port ${port}`);
