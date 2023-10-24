9db35033b552c72ae162b9f664f8f740f93ab5c5
Lauro235 <lorentzbloomguitar@gmail.com>
2023-10-24 16:12:05 +0100

    return response

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,9 @@ app.use(express.json());
 app.use(cors());
 const port = 3000;
 
-console.log(process.env.my_secret)
+app.get("/", (req, res) => {
+  res.send(`My favorite colour is ${process.env.my_secret_colour}`);
+});
 
 app.listen(port, () => {
   console.log(`Backend API Listening on port ${port}`);
