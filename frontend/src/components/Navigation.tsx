e6508658ec9d0639107ec32552459ef9b41a68cd
Lauro235 <lorentzbloomguitar@gmail.com>
2023-10-25 12:28:55 +0100

    Add photos to users and map through users
    In Navigation, users are now mapped through, rather than hardcoded.

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -9,7 +9,12 @@ import {
   // NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 
+import { useContext } from "react";
+import { UserContext } from "../main";
+
 const Navigation = () => {
+  const users = useContext(UserContext);
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -17,20 +22,17 @@ const Navigation = () => {
         <NavigationMenuItem>
           <NavigationMenuTrigger>Item 1</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <NavigationMenuLink className="p-3">
-              <p>Daniel</p>
-              <img
-                className="rounded-full aspect-square object-cover w-4/5 mx-auto"
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&q=80&w=1780&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              ></img>
-            </NavigationMenuLink>
-            <NavigationMenuLink className="p-3">
-              <p>Alison</p>
-              <img
-                className="rounded-full aspect-square object-cover w-4/5 mx-auto"
-                src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              ></img>
-            </NavigationMenuLink>
+            {users?.map((profile) => {
+              return (
+                <NavigationMenuLink className="p-3">
+                  <p>{profile.firstName}</p>
+                  <img
+                    className="rounded-full aspect-square object-cover w-4/5 mx-auto"
+                    src={profile.image}
+                  ></img>
+                </NavigationMenuLink>
+              );
+            })}
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
--- frontend/src/data/users.ts
+++ frontend/src/data/users.ts
@@ -2,34 +2,45 @@ export type TUsers = Array<{
   id: number
   firstName: string
   surname: string
+  image?: string
 }>
 
 const users: TUsers = [
   {
     id: 1,
     firstName: "Zelenia",
-    surname: "Frederick"
+    surname: "Frederick",
+    image:
+      "https://images.unsplash.com/photo-1607746882042-944635dfe10e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
     firstName: "Walker",
     surname: "Cunningham",
+    image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&q=80&w=1780&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 3,
     firstName: "Uma",
-    surname: "Scott"
+    surname: "Scott",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 4,
     firstName: "Nicholas",
-    surname: "Munoz"
+    surname: "Munoz",
+    image:
+      "https://plus.unsplash.com/premium_photo-1689533448099-2dc408030f0f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 5,
     firstName: "Summer",
-    surname: "Rivera"
-  }
+    surname: "Rivera",
+    image:
+      "https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 ];
 
 export default users;
